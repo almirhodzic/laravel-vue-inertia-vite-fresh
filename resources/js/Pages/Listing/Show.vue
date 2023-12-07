8e26305de156350a8ed0f5dc7e3c5ce617c8e764
almirh <24458972+almirhodzic@users.noreply.github.com>
2023-12-07 08:36:23 +0100

    Update Lisitings Box

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -14,9 +14,20 @@
       </Box>
       <Box>
         <template #header>
-          Offer
+          Monthly payment
         </template>
-        Make an offer
+        <div>
+          <label>Interest rate ({{ interestRate }}%)</label>
+          <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1" class="form-range" />
+
+          <label>Duration ({{ duration }} years)</label>
+          <input v-model.number="duration" type="range" min="3" max="35" step="1" class="form-range" />
+
+          <div class="text-gray-600 dark:text-gray-300 mt-2">
+            <div class="text-gray-500 mb-1">Your monthly payment</div>
+            <Price :price="monthlyPayment" class="text-3xl" />
+          </div>
+        </div>
       </Box>
     </div>
   </div>
@@ -28,10 +39,18 @@ import LisitingSpace from '@/Components/LisitingSpace.vue'
 import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
 
-defineProps({
+import { ref } from 'vue'
+import { useMonthlyPayment } from '@/Composables/useMonthlyPayment'
+
+const interestRate = ref(2.5)
+const duration = ref(25)
+
+const props = defineProps({
     listing: {
         type: Object,
         required: true,
     },
 })
+
+const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, duration)
 </script>
